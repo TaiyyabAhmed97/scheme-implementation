fd9b0f7ba980d062b77f6cabeaf7dc5c7ba6ad76
taiyyab <taiyyab.a97@gmail.com>
2022-01-17 23:51:31 -0600

    PROG-2 Implement basic functionality of atom

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+use std::io;
 
-
-struct atom {
+struct Atom {
     content: String
 }
 
 fn main() {
     println!("Hello, world!");
 
-    let x = "";
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line");
+
+    let new_atom = Atom{
+        content: input[..input.len()-1].to_owned()
+    };
+
+    is_atom(new_atom)
+}
+
+fn is_atom(atom: Atom) {
+
+    println!("{} is the input", atom.content);
+
+   match atom.content.as_ref() {
+       "hello" => println!("hello world"),
+       _ => println!("anything")
+   }   
 }
